71c5741a895ccfe6152d683ff1c27f94680b5cd2
tyulyubaev <mtulubaev@gmail.com>
2019-11-30 08:26:43 +0000

    826

--- src/components/Version.jsx
+++ src/components/Version.jsx
@@ -4,7 +4,7 @@ export default class Version extends Component {
     render() {
         return (
             <div>
-                8:18
+                8:26
             </div>
         )
     }
