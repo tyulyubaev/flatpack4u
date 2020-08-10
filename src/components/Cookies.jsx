568e5b4586a5651841edefd4513441d4b8054365
tyulyubaev <mtulubaev@gmail.com>
2020-08-11 00:45:48 +0100

    11082020-03

--- src/components/Cookies.jsx
+++ src/components/Cookies.jsx
@@ -6,15 +6,13 @@ class MessageCookies extends Component {
     return (
       <div className="object">
         <div
-          className="alert alert-secondary text-center font-weight mb-0"
+          className="alert alert-secondary text-center font-weight my-0 py-0"
           role="alert"
           id="message"
         >
           <div className="row my-auto p-0" style={{ fontSize: "0.8rem" }}>
             
-            <p className="text-left my-auto col">
-              By using this site you agree to the privacy policy
-            </p>
+            
             <button
               type="button"
               className="close col-1"
@@ -23,8 +21,11 @@ class MessageCookies extends Component {
             >
               <span aria-hidden="true">&times;</span>
             </button>
+            <p className="text-left my-auto col">
+              By using this site you agree to the privacy policy
+            </p>
           </div>
-          <div className="col-auto text-center text-sm-left  p-0 m-0"></div>
+          
         </div>
       </div>
     );
