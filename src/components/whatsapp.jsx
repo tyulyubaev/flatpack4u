0f4133932daf93af71766d814b7e63214abce8a5
tyulyubaev <mtulubaev@gmail.com>
2020-08-11 01:08:26 +0100

    11082020-04

--- src/components/whatsapp.jsx
+++ src/components/whatsapp.jsx
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import "./Popup.css";
 
-
 class Whatsapp extends Component {
   render() {
     const img = require("../assets/icons/WhatsApp.svg");
     return (
-      <div  className="sticky">
+      <div className="sticky">
         <div className="row text-right mr-1">
           <a
             className=""
@@ -15,9 +14,13 @@ class Whatsapp extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={img} alt="" className="" style={{ width: "20%" }} />
+            <img src={img} alt="" className="" style={{ width: "70px" }} />
           </a>
+          
         </div>
+        {/* <div className="row text-right">
+            <p classname="" style={{ fontSize: "0.7rem", color: "green"}}>WhatsApp</p>
+          </div> */}
       </div>
     );
   }
