23878b3fbcac2554f55d04d10f90998a69444057
tyulyubaev <mtulubaev@gmail.com>
2020-10-23 21:53:25 +0100

    23102020-02

--- src/components/home/RevCarousel.jsx
+++ src/components/home/RevCarousel.jsx
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 const imgQuotation = require("../../assets/images/quotation.svg");
 
 class reviewesCarousel extends Component {
-    render() {
-        return(
+    render() {   
+      
+        return(          
   <div className="container p-0">
     <div className="bg-secondary text-center mt-5">
     <img
@@ -19,10 +20,22 @@ class reviewesCarousel extends Component {
     <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>    
   </ol>
   <div className="carousel-inner bg-secondary text-light">
+  <div className="carousel-item active" data-interval="15000">
+      <div className="text-center m-4 px-4" >  
+        <p>    
+        "I highly recommend Michael, I have used his service 3 times and he is always on time polite and finishes work to a very high standard."
+        </p>
+        <br/>
+        <h5>Mel, Hackney</h5>
+        <br/>
+      </div> 
+    </div>
   
-    <div className="carousel-item active">       
+    <div className="carousel-item" data-interval="15000">       
       <div className="text-center m-4 px-4" >  
         <p>    
           "Michael was very professional, Personable, on time and super efficient. I never thought a wardrobe could be assembled that quickly! He offers multiple payment options which is super convenient. I strongly recommend his services."
@@ -32,7 +45,7 @@ class reviewesCarousel extends Component {
         <br/>
       </div>    
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" data-interval="15000">
     <div className="text-center m-4 px-4" >  
         <p>    
         "I would highly recommend Michael. He was very professional, timely and efficient. He assembled our furniture to a very high standard. He would be our first call if we need anything assembled."
@@ -42,8 +55,8 @@ class reviewesCarousel extends Component {
         <br/>
       </div>       
     </div>
-    <div className="carousel-item">
-    <div className="text-center m-4 px-4" >  
+    <div className="carousel-item" data-interval="15000">
+      <div className="text-center m-4 px-4" >  
         <p>    
         "Michael was brilliant! He was punctual and very professional. He made assembling wardrobes look super easy! I’m very happy with the end products! Thanks Michael!"
         </p>
@@ -52,7 +65,19 @@ class reviewesCarousel extends Component {
         <br/>
       </div> 
     </div>
+    <div className="carousel-item" data-interval="15000">
+      <div className="text-center m-4 px-4" >  
+        <p>    
+        "Micheal was punctual and professional and excellent at what he does. He is very polite, quick, easy to book and pay. He shows me how my furnitures work as is a sofa bed, dinning table thats a glass which need to be careful while lifting up and also a storage bed. Would definitely recommended him. Thanks again Micheal."
+        </p>
+        <br/>
+        <h5>Miriam, Bexleyheath</h5>
+        <br/>
+      </div> 
+    </div>
+    
   </div>
+  
   <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     <span className="sr-only">Previous</span>
@@ -67,3 +92,4 @@ class reviewesCarousel extends Component {
     }
 }
 export default reviewesCarousel;
+
